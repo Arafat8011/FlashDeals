42f8aca8fd58d8feef08a3fe0fb6c20ba87196a6
anower <azizul7m@gmail.com>
2025-08-19 14:04:24 +0600

    fetch: distribute root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,11 @@ import Footer from '../components/Footer'
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className="">
-        <Header />
-        <main className="container mx-auto px-4 py-6">{children}</main>
+      <body className="transition duration-300 ease-in-out">
+      <Header />
+        <main>
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import { products } from '../data/products'
 import ProductCard from '../components/ProductCard'
-import Slide from "@/components/Slide"
+import Dropdown from '@/components/Dropdown'
+import Slide from '@/components/Slide'
+
 
 export default function Home() {
-  return (
-    <div>
-      <Slide />
-      <h1 className="text-3xl font-bold mb-6 text-center">Featured Products</h1>
-      <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-    </div>
-  )
+    return (
+        <>
+            <Dropdown />
+            <div className="container py-6 px-4 mx-auto">
+                <Slide />
+              <h1 className="mb-6 text-3xl font-bold text-center rounded-b-md shodow">Featured Products</h1>
+                <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3"  >
+                    {products.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+                </div>
+            </div>
+        </>
+    )
 }

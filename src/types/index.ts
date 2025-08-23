5a627001f967bc60cb99a7a1fa1ed8dc77428106
anower <azizul7m@gmail.com>
2025-08-23 13:25:08 +0600

    added: Product types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,20 @@
-type productsTypes = {
+export interface ProductTypes {
   id: string,
-  name: string
-  brand: string
-  price: number
-  currency: string
+  name: string,
+  brand: string,
+  price: number,
+  currency: string,
   stock: number,
+  category: string,
+  subCategory: string,
   rating: number,
   description: string,
   image: string,
-  specifications: {
-    processor: string,
-    ram: string,
-    storage: string,
-    display: string
-  }
+  specifications: ProductSpecifications,
+}
+export interface ProductSpecifications {
+  processor: string,
+  ram: string,
+  storage: string,
+  display: string
 }

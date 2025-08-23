7d1f378a22668a640240c45ddbf275c42ad451be
anower <azizul7m@gmail.com>
2025-08-23 11:18:38 +0600

    fix: colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-white mt-8 shadow-inner">
-      <div className="container mx-auto px-4 py-4 text-center text-gray-600">
+    <footer className="bg-surface text-primary mt-8 shadow-inner">
+      <div className="container mx-auto px-4 py-4 text-center">
         &copy; 2025 FlashDeals. All rights reserved.
       </div>
     </footer>

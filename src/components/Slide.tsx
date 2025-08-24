9921ab78928894de73ba1675cd0a54bb8d59d67c
anower <azizul7m@gmail.com>
2025-08-24 07:02:23 +0600

    added: slider

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,10 +1,12 @@
 "use client"
-import { useState } from "react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const images = [
+
   {
     id: 0,
-    url: "/slide/1.png"
+    url: "/slide/test.jpg"
   },
   {
     id: 1,
@@ -25,18 +27,52 @@ const images = [
   {
     id: 5,
     url: "/slide/5.png"
+  },
+  {
+    id: 6,
+    url: "/slide/6.png"
   }
 ];
 
 const Slide = () => {
-  const [img, setImg] = useState("");
-  const handleNext = () => {
-    // TODO: add logic
+  const [currentIndex, setCurrentIndex] = useState(1);
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    )
   }
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex + 1
+    )
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 8000); // Changed to 2 seconds
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="shadow rounded-md">
-      <div className="flex items-center text-center ">
-        <img className="h-auto max-w-full overflow-hidden rounded-md" src="/slide/1.png" alt="" />
+    <div className="shadow ">
+      <div className="grid md:grid-cols-3 grid-rows-2 gap-2 object-cover">
+        <div className={`col-span-2 row-span-2 relative overflow-hidden`}>
+          {images.map((img, index) => (
+            <div key={index} className={`absolute transition-opacity duration-700 
+                ${index === currentIndex ? 'opacity-100' : "opacity-0"}`}>
+              <img src={img.url} alt="" />
+            </div>
+          ))}
+          <button className="absolute left-8 top-1/2 cursor-pointer text-primary hover:bg-button-hover hover:text-primary-fg hover:rounded-full active:bg-button-active hover:scale-110 active:scale-95" onClick={() => prevSlide()}><ArrowLeft size={30} /></button>
+          <button className="absolute right-8 top-1/2 cursor-pointer text-primary hover:bg-button-hover hover:text-primary-fg hover:rounded-full active:bg-button-active hover:scale-110 active:scale-95" onClick={() => nextSlide()}><ArrowRight size={30} /></button>
+        </div>
+        <img className="h-auto max-w-full w-full mx-auto overflow-hidden " src="/slide/test.jpg" alt="" />
+        <img className="h-auto max-w-full w-full mx-auto overflow-hidden " src="/slide/test.jpg" alt="" />
       </div>
     </div>
   )
